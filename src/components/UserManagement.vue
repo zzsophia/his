da0a62a1ab5e6e731ad29722ba908301987e6eb9
zzsophia <13644080621@163.com>
2019-11-01 16:18:03 +0800

    用户管理

---
+++ src/components/UserManagement.vue
@@ -0,0 +1,177 @@
+<template>
+    <div>
+        <div class="el-col el-col-6" data-v-49055fb4="">
+            <div class="grid-content bg-purple" data-v-49055fb4="">
+                <div class="el-input el-input--mini" data-v-49055fb4="">
+                    <input class="el-input__inner" type="text"
+                           placeholder="请输入登录名或姓名" autocomplete="off"><!---->
+                </div>
+            </div>
+        </div>
+        <div class="el-col el-col-6" data-v-49055fb4="">
+            <div class="grid-content bg-purple" data-v-49055fb4="">
+                <button class="el-button el-button--primary el-button--mini"
+                        style="margin-left: 10px;" type="button" data-v-49055fb4="">
+                    <i class="el-icon-view"></i><span>查询用户</span></button>
+            </div>
+        </div>
+        <div class="el-col el-col-6" data-v-49055fb4="">
+            <div class="grid-content bg-purple" data-v-49055fb4="">
+                <button class="el-button el-button--primary el-button--mini"
+                        style="margin-left: 10px;" type="button" data-v-49055fb4="">
+                    <i class="el-icon-edit"></i><span>新增用户</span></button>
+            </div>
+        </div>
+        <div class="el-table el-table--fit el-table--striped el-table--scrollable-x el-table--enable-row-hover el-table--enable-row-transition el-table--mini"
+             style="width: 100%;" data-v-49055fb4="">
+            <div class="hidden-columns">
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+            </div>
+            <div class="el-table__header-wrapper">
+                <table class="el-table__header" style="width: 815px;" border="0"
+                       cellspacing="0" cellpadding="0">
+                    <colgroup>
+                        <col width="55" name="el-table_3_column_18">
+                        <col width="140" name="el-table_3_column_19">
+                        <col width="80" name="el-table_3_column_20">
+                        <col width="140" name="el-table_3_column_21">
+                        <col width="80" name="el-table_3_column_22">
+                        <col width="80" name="el-table_3_column_23">
+                        <col width="80" name="el-table_3_column_24">
+                        <col width="80" name="el-table_3_column_25">
+                        <col width="80" name="el-table_3_column_26">
+                        <col width="0" name="gutter">
+                    </colgroup>
+                    <thead class="has-gutter">
+                    <tr>
+                        <th class="el-table_3_column_18  is-left el-table-column--selection  is-leaf"
+                            rowspan="1" colspan="1">
+                            <div class="cell"><label class="el-checkbox"
+                                                     role="checkbox"><span
+                                    class="el-checkbox__input"
+                                    aria-checked="mixed"><input
+                                    class="el-checkbox__original" aria-hidden="true"
+                                    type="checkbox" value=""></span><!----></label>
+                            </div>
+                        </th>
+                        <th class="el-table_3_column_19  is-left   is-leaf" rowspan="1"
+                            colspan="1">
+                            <div class="cell">登录名</div>
+                        </th>
+                        <th class="el-table_3_column_20  is-left   is-leaf" rowspan="1"
+                            colspan="1">
+                            <div class="cell">真实姓名</div>
+                        </th>
+                        <th class="el-table_3_column_21  is-left   is-leaf" rowspan="1"
+                            colspan="1">
+                            <div class="cell">用户类别</div>
+                        </th>
+                        <th class="el-table_3_column_22  is-left   is-leaf" rowspan="1"
+                            colspan="1">
+                            <div class="cell">医生职称</div>
+                        </th>
+                        <th class="el-table_3_column_23  is-left   is-leaf" rowspan="1"
+                            colspan="1">
+                            <div class="cell">所在科室</div>
+                        </th>
+                        <th class="el-table_3_column_24  is-left   is-leaf" rowspan="1"
+                            colspan="1">
+                            <div class="cell">挂号级别</div>
+                        </th>
+                        <th class="el-table_3_column_26  is-center   is-leaf"
+                            rowspan="1" colspan="1">
+                            <div class="cell">操作</div>
+                        </th>
+                        <th class="gutter" style="width: 0px; display: none;"></th>
+                    </tr>
+                    </thead>
+                </table>
+            </div>
+            <div class="el-table__body-wrapper is-scrolling-left">
+                <table class="el-table__body" style="width: 815px;" border="0"
+                       cellspacing="0" cellpadding="0">
+                    <colgroup>
+                        <col width="55" name="el-table_3_column_18">
+                        <col width="140" name="el-table_3_column_19">
+                        <col width="80" name="el-table_3_column_20">
+                        <col width="140" name="el-table_3_column_21">
+                        <col width="80" name="el-table_3_column_22">
+                        <col width="80" name="el-table_3_column_23">
+                        <col width="80" name="el-table_3_column_24">
+                        <col width="80" name="el-table_3_column_25">
+                        <col width="80" name="el-table_3_column_26">
+                    </colgroup>
+                    <tbody><tr class="el-table__row">
+                        <td class="el-table_3_column_18 is-left el-table-column--selection"
+                            rowspan="1" colspan="1">
+                            <div class="cell"><label class="el-checkbox"
+                                                     role="checkbox"><span
+                                    class="el-checkbox__input"
+                                    aria-checked="mixed"><input
+                                    class="el-checkbox__original" aria-hidden="true"
+                                    type="checkbox" value=""></span></label>
+                            </div>
+                        </td>
+                        <td class="el-table_3_column_19 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">张仲景</div>
+                        </td>
+                        <td class="el-table_3_column_20 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">张仲景</div>
+                        </td>
+                        <td class="el-table_3_column_21 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">门诊医生</div>
+                        </td>
+                        <td class="el-table_3_column_22 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">主任医师</div>
+                        </td>
+                        <td class="el-table_3_column_23 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">小儿耳鼻喉科</div>
+                        </td>
+                        <td class="el-table_3_column_24 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">普通号</div>
+                        </td>
+                        <td class="el-table_3_column_26 is-center " rowspan="1"
+                            colspan="1">
+                            <div class="cell">
+                                <button class="el-button el-button--primary el-button--mini"
+                                        type="button" data-v-49055fb4=""><!---->
+                                    <span>编辑
+          </span></button>
+                                <button class="el-button el-button--danger el-button--mini"
+                                        type="button" data-v-49055fb4=""><!---->
+                                    <span>删除
+          </span></button>
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="el-table__column-resize-proxy" style="display: none;"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "UserManagement"
+    }
+</script>
+
+<style scoped>
+
+</style>
