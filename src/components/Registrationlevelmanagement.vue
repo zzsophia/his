f892e946296c541cd9bc08b71eb3369dc7f1b2c3
zzsophia <13644080621@163.com>
2019-11-01 16:19:17 +0800

    挂号级别管理

---
+++ src/components/Registrationlevelmanagement.vue
@@ -0,0 +1,214 @@
+<template>
+    <div>
+        <div class="el-col el-col-6" data-v-49055fb4="">
+            <div class="grid-content bg-purple" data-v-49055fb4="">
+                <div class="el-input el-input--mini" data-v-49055fb4="">
+                    <input class="el-input__inner" type="text"
+                           placeholder="请输入挂号级别编码或名称" autocomplete="off"><!---->
+                </div>
+            </div>
+        </div>
+        <div class="el-col el-col-6" data-v-49055fb4="">
+            <div class="grid-content bg-purple" data-v-49055fb4="">
+                <button class="el-button el-button--primary el-button--mini"
+                        style="margin-left: 10px;" type="button" data-v-49055fb4="">
+                    <i class="el-icon-view"></i><span>查询挂号级别</span></button>
+            </div>
+        </div>
+        <div class="el-col el-col-6" data-v-49055fb4="">
+            <div class="grid-content bg-purple" data-v-49055fb4="">
+                <button class="el-button el-button--primary el-button--mini"
+                        style="margin-left: 10px;" type="button" data-v-49055fb4="">
+                    <i class="el-icon-edit"></i><span>新增挂号级别</span></button>
+            </div>
+        </div>
+        <div class="el-table el-table--fit el-table--striped el-table--scrollable-x el-table--enable-row-hover el-table--enable-row-transition el-table--mini"
+             style="width: 100%;" data-v-49055fb4="">
+            <div class="hidden-columns">
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+                <div data-v-49055fb4=""></div>
+            </div>
+            <div class="el-table__header-wrapper">
+                <table class="el-table__header" style="width: 815px;" border="0"
+                       cellspacing="0" cellpadding="0">
+                    <colgroup>
+                        <col width="55" name="el-table_3_column_18">
+                        <col width="140" name="el-table_3_column_19">
+                        <col width="80" name="el-table_3_column_20">
+                        <col width="140" name="el-table_3_column_21">
+                        <col width="80" name="el-table_3_column_22">
+                        <col width="80" name="el-table_3_column_23">
+                        <col width="80" name="el-table_3_column_24">
+                        <col width="80" name="el-table_3_column_25">
+                        <col width="80" name="el-table_3_column_26">
+                        <col width="0" name="gutter">
+                    </colgroup>
+                    <thead class="has-gutter">
+                    <tr>
+                        <th class="el-table_3_column_18  is-left el-table-column--selection  is-leaf"
+                            rowspan="1" colspan="1">
+                            <div class="cell"><label class="el-checkbox"
+                                                     role="checkbox"><span
+                                    class="el-checkbox__input"
+                                    aria-checked="mixed"><input
+                                    class="el-checkbox__original" aria-hidden="true"
+                                    type="checkbox" value=""></span><!----></label>
+                            </div>
+                        </th>
+                        <th class="el-table_3_column_19  is-left   is-leaf" rowspan="1"
+                            colspan="1">
+                            <div class="cell">显示顺序号</div>
+                        </th>
+                        <th class="el-table_3_column_20  is-left   is-leaf" rowspan="1"
+                            colspan="1">
+                            <div class="cell">号别编码</div>
+                        </th>
+                        <th class="el-table_3_column_21  is-left   is-leaf" rowspan="1"
+                            colspan="1">
+                            <div class="cell">号别名称</div>
+                        </th>
+                        <th class="el-table_3_column_22  is-left   is-leaf" rowspan="1"
+                            colspan="1">
+                            <div class="cell">挂号费</div>
+                        </th>
+                        <th class="el-table_3_column_23  is-left   is-leaf" rowspan="1"
+                            colspan="1">
+                            <div class="cell">挂号限额</div>
+                        </th>
+                        <th class="el-table_3_column_26  is-center   is-leaf"
+                            rowspan="1" colspan="1">
+                            <div class="cell">操作</div>
+                        </th>
+                        <th class="gutter" style="width: 0px; display: none;"></th>
+                    </tr>
+                    </thead>
+                </table>
+            </div>
+            <div class="el-table__body-wrapper is-scrolling-left">
+                <table class="el-table__body" style="width: 815px;" border="0"
+                       cellspacing="0" cellpadding="0">
+                    <colgroup>
+                        <col width="55" name="el-table_3_column_18">
+                        <col width="140" name="el-table_3_column_19">
+                        <col width="80" name="el-table_3_column_20">
+                        <col width="140" name="el-table_3_column_21">
+                        <col width="80" name="el-table_3_column_22">
+                        <col width="80" name="el-table_3_column_23">
+                        <col width="80" name="el-table_3_column_24">
+                        <col width="80" name="el-table_3_column_25">
+                        <col width="80" name="el-table_3_column_26">
+                    </colgroup>
+                    <tbody><tr class="el-table__row">
+                        <td class="el-table_3_column_18 is-left el-table-column--selection"
+                            rowspan="1" colspan="1">
+                            <div class="cell"><label class="el-checkbox"
+                                                     role="checkbox"><span
+                                    class="el-checkbox__input"
+                                    aria-checked="mixed"><input
+                                    class="el-checkbox__original" aria-hidden="true"
+                                    type="checkbox" value=""></span></label>
+                            </div>
+                        </td>
+                        <td class="el-table_3_column_19 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">2</div>
+                        </td>
+                        <td class="el-table_3_column_20 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">pth</div>
+                        </td>
+                        <td class="el-table_3_column_21 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">普通号</div>
+                        </td>
+                        <td class="el-table_3_column_22 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">8</div>
+                        </td>
+                        <td class="el-table_3_column_23 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">30</div>
+                        </td>
+                        <td class="el-table_3_column_26 is-center " rowspan="1"
+                            colspan="1">
+                            <div class="cell">
+                                <button class="el-button el-button--primary el-button--mini"
+                                        type="button" data-v-49055fb4=""><!---->
+                                    <span>编辑
+          </span></button>
+                                <button class="el-button el-button--danger el-button--mini"
+                                        type="button" data-v-49055fb4=""><!---->
+                                    <span>删除
+          </span></button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="el-table__row">
+                        <td class="el-table_3_column_18 is-left el-table-column--selection"
+                            rowspan="1" colspan="1">
+                            <div class="cell"><label class="el-checkbox"
+                                                     role="checkbox"><span
+                                    class="el-checkbox__input"
+                                    aria-checked="mixed"><input
+                                    class="el-checkbox__original" aria-hidden="true"
+                                    type="checkbox" value=""></span></label>
+                            </div>
+                        </td>
+                        <td class="el-table_3_column_19 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">2</div>
+                        </td>
+                        <td class="el-table_3_column_20 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">pth</div>
+                        </td>
+                        <td class="el-table_3_column_21 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">普通号</div>
+                        </td>
+                        <td class="el-table_3_column_22 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">8</div>
+                        </td>
+                        <td class="el-table_3_column_23 is-left " rowspan="1"
+                            colspan="1">
+                            <div class="cell">30</div>
+                        </td>
+                        <td class="el-table_3_column_26 is-center " rowspan="1"
+                            colspan="1">
+                            <div class="cell">
+                                <button class="el-button el-button--primary el-button--mini"
+                                        type="button" data-v-49055fb4=""><!---->
+                                    <span>编辑
+          </span></button>
+                                <button class="el-button el-button--danger el-button--mini"
+                                        type="button" data-v-49055fb4=""><!---->
+                                    <span>删除
+          </span></button>
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="el-table__column-resize-proxy" style="display: none;"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Registrationlevelmanagement"
+    }
+</script>
+
+<style scoped>
+
+</style>
